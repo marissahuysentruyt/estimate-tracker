2155f95678401ad20513f87ee13ad49c305d599b
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-04-23 14:45:25 -0400

    refactor: AboutView is now DetailsView

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/public/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="@/public/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/details">Details</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/DetailsView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="details">
+    <h1>This page will eventually show all the inputs from a particular task or project.</h1>
+  </div>
+</template>
+
