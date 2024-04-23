36f4653ab14c231a228c928c00804a80bc68749c
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-04-23 09:49:02 -0400

    feat: add cypress github action with reporting

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,17 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
-  }
-})
+    baseUrl: 'http://localhost:5173',
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message)
+          return null
+        }
+      })
+    },
+    // Enables the "Run All Specs" UI feature, allowing the execution of multiple specs sequentially.
+    experimentalRunAllSpecs: true,
+    video: false
+  },
+});
