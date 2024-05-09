5005b1f97fec1ecf01b1e40ef1d5026db5761f49
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-05-09 13:21:44 -0400

    feat: HomeView has h1 and InputForm components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import InputForm from '@/components/InputForm.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Estimate Tracker</h1>
+    <InputForm />
   </main>
 </template>
