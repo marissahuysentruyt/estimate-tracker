afa363de771ac66321245e49c7bddcf8ac463954
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-05-09 13:23:48 -0400

    feat: creates InputItem component

---
+++ src/components/InputItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  defineProps<{
+    label: string,
+    name: string
+  }>()
+
+  const inputValue = ref(''); 
+</script>
+
+<template>
+  <label>
+    *{{ label }}:
+    <input required type="text" :name="name" v-model="inputValue">
+  </label>
+</template>
