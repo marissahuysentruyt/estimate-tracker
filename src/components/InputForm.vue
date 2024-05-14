fad1a0a8cef11124daa4d22020a9ced0752123e1
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-05-14 15:25:48 -0400

    feat: render list of input values after submitting form

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,32 +1,52 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
+  import type { Ref } from 'vue';
   import InputItem from './InputItem.vue';
   import SubmitButton from './SubmitButton.vue';
+
+  let formSubmitted = ref(false);
+  const setFormSubmitted = (currentState: Ref<boolean>) => {
+    formSubmitted.value = !currentState.value;
+    return formSubmitted;
+  }
 
   const inputValueArray: string[] = [];
 
   const getInputValues = (inputs: NodeListOf<HTMLInputElement>): void => {
     for (let i = 0; i < inputs.length; i++) {
-      let input = inputs[i];
-      inputValueArray.push(input.value)
+      const input = inputs[i];
+      const value = inputs[i].value;
+      if (!inputValueArray.includes(value)) {
+        inputValueArray.push(input.value)
+      } else {
+        return;
+      }
     }
+    setFormSubmitted(formSubmitted);
   }
 
-  const handleSubmit = (): string[] => {
-    const inputValues = document.querySelectorAll('input');
-    getInputValues(inputValues);
-    return inputValueArray;
+  // this `any` is referencing each of the InputItems
+  const handleInputChanges = (inputElement: any) => {
+    inputValueArray.push(inputElement.srcElement?.value)
   }
 </script>
 
 <template>
-  <form @submit.prevent="handleSubmit">
-    <InputItem label="Task" name="task" />
-    <InputItem label="Estimated Time" name="estimation"/>
-    <InputItem label="Description" name="task-description"/>
-    <InputItem label="Actual Time" name="actual-time"/>
+  <form @submit.prevent>
+    <InputItem label="Task" name="task" @change="handleInputChanges" />
+    <InputItem label="Estimated Time" name="estimation" @change="handleInputChanges" />
+    <InputItem label="Description" name="task-description" @change="handleInputChanges"/>
+    <InputItem label="Actual Time" name="actual-time" @change="handleInputChanges"/>
 
-    <SubmitButton buttonText="Save" />
+    <SubmitButton buttonText="Save" @click="getInputValues" />
   </form>
 
-  <!-- <p>{{ value }}</p> -->
+  <h2>Your Estimates</h2>
+  <ul v-if="formSubmitted">
+    <template v-for="inputValue in inputValueArray" :key="inputValue.indexOf">
+      <li>
+        {{ inputValue }}
+      </li>
+    </template>
+  </ul>
 </template>
