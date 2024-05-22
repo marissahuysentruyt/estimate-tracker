50f24c24eccd1dbfe8110a29b06f6e1fb24d75e7
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-05-22 09:55:47 -0400

    test: spectrum button

--- src/components/SubmitButton.vue
+++ src/components/SubmitButton.vue
@@ -2,10 +2,17 @@
   defineProps<{
     buttonText: string;
   }>();
+
+  import "../../node_modules/@spectrum-css/tokens/dist/index.css";
+  import "../../node_modules/@spectrum-css/typography/dist/index.css";
+// import "../../node_modules/@spectrum-css/icon/dist/index.css";
+  import "../../node_modules/@spectrum-css/button/dist/index.css";
+  import "../../node_modules/@spectrum-css/button/dist/index-base.css";
+  import "../../node_modules/@spectrum-css/button/dist/themes/spectrum.css";
 </script>
 
 <template>
-  <button type="submit">
-    {{ buttonText }}
+  <button type="submit" class="spectrum-Button spectrum-Button--outline spectrum-Button--sizeS spectrum-Button--secondary">
+    <span class="spectrum-Button-label">{{ buttonText }}</span>
   </button>
 </template>
